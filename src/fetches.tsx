8035dac70469fad8583664fb492118594db74df5
Adam Meza <Adammeza95@gmail.com>
2023-06-10 07:24:56 -0400

    edited error handling

--- src/fetches.tsx
+++ src/fetches.tsx
@@ -1,22 +1,29 @@
 export const fetchDefinition = async (word : String) => {
   try {
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-    if (!response.ok) {
+    const json = await response.json();
+
+    if (response.status >= 400 && response.status <= 599) {
       throw new Error('Network response was not OK');
     }
-    return response.json();
+
+    return json;
   } catch (error) {
     throw error;
   }
 };
+
 export const fetchLetters = async () => {
   try {
     const response = await fetch('https://freebee.fun/cgi-bin/today');
-    if (!response.ok) {
+    const json = await response.json();
+
+    if (response.status >= 400 && response.status <= 599) {
       throw new Error('Network response was not OK');
     }
-    return response.json();
+
+    return json;
   } catch (error) {
-    return error;
+    throw error;
   }
 };
